3c11bf961158893f77e245dc18f3ce8fe148225a
Samriddhi Srivastava <35305134+samriddhi-99@users.noreply.github.com>
2019-10-10 15:27:56 +0530

    Update fibonacci.c

--- fibonacci.c
+++ fibonacci.c
@@ -1,12 +1,13 @@
+#include <stdio.h>
 
 // Fibonacci Series using Recursion
-#include <stdio.h>
 int fib(int n)
 {
     if (n <= 1)
         return n;
     return fib(n - 1) + fib(n - 2);
 }
+
 //Tested progam
 int main()
 {
